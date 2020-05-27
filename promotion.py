5f58d1c52f4d4265f0f426c36d71d512c42454d7
The Oracle Software Owner User <oracle@blk-vm.sub05261508050.blkvcn.oraclevcn.com>
2020-05-27 20:41:55 +0000

    Convert application to web service

--- promotion.py
+++ promotion.py
@@ -2,14 +2,21 @@
 Simple Python application to show CI/CD capabilities.
 """
 
+from bottle import Bottle, run
 
+app = Bottle()
+
+@app.rout('/addition/<salary>/<amount>') 
 def addition(salary, amount):
     return salary + amount
 
-
+@app.rout('/increment/<salary>/<percentage>')
 def increment(salary, percentage):
     return salary * (1 + percentage/100)
 
-
+@app.rout('/decrease/<salary>/<amount>')
 def decrease(salary, amount):
     return salary - amount
+
+if _name_ = '_main_':
+  run(app,host='0.0.0.0', port=8080)
