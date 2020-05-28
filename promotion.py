07978fd5d95201ae544540f69d498e92756e75db
The Oracle Software Owner User <oracle@blk-vm.sub05261508050.blkvcn.oraclevcn.com>
2020-05-28 01:44:09 +0000

    Fixed Route Error

--- promotion.py
+++ promotion.py
@@ -31,7 +31,7 @@ def increment(salary, percentage):
     return salary * (1 + percentage/100)
 
 # run(app, host='0.0.0.0', port=8080)
- 
+
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080, debug=True)
