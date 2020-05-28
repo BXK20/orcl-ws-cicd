69b22f6e18a0daaca26d3c355527c3145a8f3667
The Oracle Software Owner User <oracle@blk-vm.sub05261508050.blkvcn.oraclevcn.com>
2020-05-28 01:34:45 +0000

    Fixed Route Error

--- promotion.py
+++ promotion.py
@@ -2,27 +2,35 @@
 Simple Python application to show CI/CD capabilities.
 """
 
-import bottle
-
-from bottle import Bottle, run
-
-app = Bottle()
+#from bottle import Bottle, run
+from bottle import rout,run
+#app = Bottle()
 
 
-@app.rout('/decrease/<salary>/<amount>')
+#@app.rout('/decrease/<salary>/<amount>')
+
+
+@rout('/decrease/<salary>/<amount>')
 def decrease(salary, amount):
     return salary - amount
 
 
-@app.rout('/addition/<salary>/<amount>')
+#@app.rout('/addition/<salary>/<amount>')
+
+
+@rout('/addition/<salary>/<amount>')
 def addition(salary, amount):
     return salary + amount
 
 
-@app.rout('/increment/<salary>/<percentage>')
+#@app.rout('/increment/<salary>/<percentage>')
+
+
+@rout('/increment/<salary>/<percentage>')
 def increment(salary, percentage):
     return salary * (1 + percentage/100)
 
 
 if __name__ == '__main__':
-    run(app, host='0.0.0.0', port=8080)
+#    run(app, host='0.0.0.0', port=8080)
+    run(host='0.0.0.0', port=8080, debug=True)
