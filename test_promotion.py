af3bcab1623e8b56cfd4f939bef0763f304377c2
The Oracle Software Owner User <oracle@blk-vm.sub05261508050.blkvcn.oraclevcn.com>
2020-05-28 02:43:25 +0000

    Add unit tests for web service

--- test_promotion.py
+++ test_promotion.py
@@ -15,3 +15,19 @@ class TestPromotion:
 
     def test_increment(self):
         assert 1250 == promotion.increment(1000, 25)
+
+
+@pytest.fixture
+def application():
+    test_app = TestApp(promotion.app)
+    return test_app
+
+
+def test_response_shold_be_ok(application):
+    response = application.get('/addition/1000/200')
+    assert response.status == "200 OK"
+
+
+def test_addition(application):
+    response = application.get('/addition/1000/200')
+    assert b'1200' == response.body
